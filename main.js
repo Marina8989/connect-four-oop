4270df973c555c3b2e180b1edbd095344e0a7b95
Marina8989 <marina.mill209@gmail.com>
2020-08-23 14:54:49 -0700

    connect four oop

--- main.js
+++ main.js
@@ -30,5 +30,74 @@ class Game{
           top.append(headCell);
         }
         board.append(top);
+
+        for(let y = 0; y < this.height; y++) {
+           const row = document.createElement('tr');
+
+           for(let x = 0; x < this.width; x++) {
+            const cell = document.createElement('td');
+            cell.setAttribute('id', `${y}-${x}`);
+            row.append(cell);
+           }
+           board.append(row);
+        }
     }
+
+     findSpotForCol(x) {
+         for(let y = this.height - 1; y >= 0; y--) {
+           if(!this.board[y][x]) {
+               return y;
+           }
+         }
+         return null;
+     }
+      placeInTable(y, x) {
+        const piece = document.createElement('div');
+        piece.classList.add('piece');
+        piece.style.backgroundColor = this.currPlayer.color;
+        piece.style.top = 50 * (y + 2);
+
+        const spot = document.getElementById(`${y}-${x}`);
+        spot.append(piece);
+      }
+      
+      endGame(msg) {
+          alert(msg);
+          const top = document.querySelector('#column-top');
+          top.removeEventListener('click', this.handleGameClick);
+      }
+      handleClick(evt) {
+          const x = +evt.target.id;
+
+          const y = this.findSpotForCol(x);
+          if(y === null) {
+              return;
+          }
+          this.board[y][x] = this.currPlayer;
+          this.placeInTable(y, x);
+
+          if(this.board.every(row => row.every(cell => cell))) {
+            return this.endGame('Tie');
+          }
+          if(this.checkForWin()) {
+            this.gameOver = true;
+            return this.endGame(`The ${this.currPlayer.color} player won`);
+          }
+          this.currPlayer = this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
+      }
+      checkForWin() {
+          const _win = cell => cells.every(
+              ([y, x]) => y >= 0 &&
+              y < this.height &&
+              x >= 0 &&
+              x < this.width &&
+              this.board[y][x] === this.currPlayer
+          );
+          for(let y = 0; y < this.height; y++) {
+             for(let x = 0; x < this.width; x++) {
+               const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+             }
+          }
+      }
+
 }
